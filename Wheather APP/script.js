cc24c8c90ea540131e3e390766e33e9c3a26a45e
aniket <apandey477@rku.ac.in>
2025-02-04 21:51:17 +0530

    fetched api key

--- Wheather APP/script.js
+++ Wheather APP/script.js
@@ -1,2 +1,12 @@
-const apiKey = "";
-const apiUrl = "";
+const apiKey = "eb6aae61650271d339a2126340f56538";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=banglore";
+
+async function checkWheather(){
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+
+}
+
+checkWheather();
