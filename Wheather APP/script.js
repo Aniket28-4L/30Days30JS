cbf0101d012b4fca7c0ca2f7cf858f0173c3cddb
aniket <apandey477@rku.ac.in>
2025-02-04 21:58:17 +0530

    did modification in js

--- Wheather APP/script.js
+++ Wheather APP/script.js
@@ -1,11 +1,17 @@
 const apiKey = "eb6aae61650271d339a2126340f56538";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=banglore";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
 
 async function checkWheather(){
     const response = await fetch(apiUrl + `&appid=${apiKey}`);
     var data = await response.json();
 
     console.log(data);
+
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round (data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
 }
 
