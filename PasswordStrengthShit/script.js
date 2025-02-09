b1109de73cf385b925ab6a0aa125b54929483b3e
aniket <apandey477@rku.ac.in>
2025-02-09 22:16:05 +0530

    Password Strength can be tested now

--- PasswordStrengthShit/script.js
+++ PasswordStrengthShit/script.js
@@ -9,12 +9,19 @@ pass.addEventListener("input",()=>{
         msg.style.display = "block";
     }
     if(pass.value.length < 4){
-        str.innerHTML = "weak"
+        str.innerHTML = "weak";
+        pass.style.borderColor = "#ff5925";
+        msg.style.color = "#ff5925";
     }
     else if(pass.value.length >= 4 && pass.value.length < 8){
-        str.innerHTML = "medium"
+        str.innerHTML = "medium";
+        pass.style.borderColor = "yellow";
+        msg.style.color = "yellow";
     }
-    else if(pass.value.length >= 0){
-        str.innerHTML = "strong"
+    else if(pass.value.length >= 8){
+        str.innerHTML = "strong";
+        pass.style.borderColor = "#26d730";
+        msg.style.color = "#26d730";
+        
     }
 })
