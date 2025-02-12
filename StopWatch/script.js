1a8edd5b62469385b6c5336f40f5c913b0c926f9
aniket <apandey477@rku.ac.in>
2025-02-12 22:38:03 +0530

    Completed JavaScript for StopWatch

--- StopWatch/script.js
+++ StopWatch/script.js
@@ -26,3 +26,14 @@ function watchStart(){
 }
 
 
+function watchStop(){
+    clearInterval(timer);
+    
+}
+
+function watchReset(){
+    clearInterval(timer);
+    [seconds, minutes, hours] = [0, 0, 0];
+    displayTime.innerHTML = "00:00:00";
+    
+}
