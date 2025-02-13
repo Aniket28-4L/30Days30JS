b0ddd425fa6834d2eeed719e77a7c26fde05374c
aniket <apandey477@rku.ac.in>
2025-02-13 15:32:26 +0530

    added some questions

---
+++ QuizApp/script.js
@@ -0,0 +1,129 @@
+const questions = [
+    {
+        question: "What is the capital of France?",
+        answers: [{
+            text: "Berlin",
+            correct: false
+        },
+        {
+            text: "Madrid",
+            correct: false
+        },
+        {
+            text: "Paris",
+            correct: true
+        },
+        {
+            text: "Rome",
+            correct: false
+        },
+        ]
+        
+    },
+    {
+        question: "What is the capital of Germany?",
+        answers: [{
+            text: "Berlin",
+            correct: true
+        },
+        {
+            text: "Madrid",
+            correct: false
+        },
+        {
+            text: "Paris",
+            correct: false
+        },
+        {
+            text: "Rome",
+            correct: false
+        },
+        ]
+        
+    },
+    {
+        question: "What is the capital of Spain?",
+        answers: [{
+            text: "Berlin",
+            correct: false
+        },
+        {
+            text: "Madrid",
+            correct: true
+        },
+        {
+            text: "Paris",
+            correct: false
+        },
+        {
+            text: "Rome",
+            correct: false
+        },
+        ]
+        
+    },
+    {
+        question: "What is the capital of Italy?",
+        answers: [{
+            text: "Berlin",
+            correct: false
+        },
+        {
+            text: "Madrid",
+            correct: false
+        },
+        {
+            text: "Paris",
+            correct: false
+        },
+        {
+            text: "Rome",
+            correct: true
+        },
+        ]
+        
+    },
+    {
+        question: "What is the capital of Portugal?",
+        answers: [{
+            text: "Berlin",
+            correct: false
+        },
+        {
+            text: "Madrid",
+            correct: false
+        },
+        {
+            text: "Lisbon",
+            correct: true
+        },
+        {
+            text: "Rome",
+            correct: false
+        },
+        ]
+        
+    },
+    {
+        question: "What is the capital of the Netherlands?",
+        answers: [{
+            text: "Amsterdam",
+            correct: true
+        },
+        {
+            text: "Madrid",
+            correct: false
+        },
+        {
+            text: "Paris",
+            correct: false
+        },
+        {
+            text: "Rome",
+            correct: false
+        },
+        ]
+        
+    }
+
+];
