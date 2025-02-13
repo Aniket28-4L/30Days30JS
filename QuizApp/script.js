ea5a49aabad9295b637a2cd34488a54c43381372
aniket <apandey477@rku.ac.in>
2025-02-13 22:28:35 +0530

    Now select the answer

--- QuizApp/script.js
+++ QuizApp/script.js
@@ -176,7 +176,15 @@ function selectAnswer(e){
         selectedBtn.classList.add('correct');
     }else{
         selectedBtn.classList.add('incorrect');
+    
     }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === 'true'){
+            button.classList.add('correct');
+        }
+        button.disabled = true;
+    })
+    nextButton.style.display = 'block';
 }
 
 
