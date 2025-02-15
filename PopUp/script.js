0abd3117a74fdb5d6017a8506b1ec51745b25d40
aniket <apandey477@rku.ac.in>
2025-02-15 22:27:27 +0530

    added Popup

--- PopUp/script.js
+++ PopUp/script.js
@@ -2,5 +2,8 @@ let popup = document.getElementById('popup');
 
 
 function openPopup(){
-    
+    popup.classList.add('open-popup');
+}
+function closePopup(){
+    popup.classList.remove('open-popup');
 }
