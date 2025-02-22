14c25da533ff053dd99dbde2053b270e278f251e
aniket <apandey477@rku.ac.in>
2025-02-22 22:26:52 +0530

    success message

--- WorkingEmail/script.js
+++ WorkingEmail/script.js
@@ -1,10 +1,13 @@
 
   const scriptURL = 'https://script.google.com/macros/s/AKfycbykme3qvNMUS-NEQFW-Z_4BjAmoyo3viZO546NU3vXfOfk7UQqH3vHCr4vmY_o2DP_A/exec'
   const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById('msg')
 
   form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
+      .then(response => {
+        msg.innerHTML = 'Thank You For Subscribing!';
+      })
       .catch(error => console.error('Error!', error.message))
   })
