e4a684b3c8c1d4578b4bb3efaaa7a7981c263143
aniket <apandey477@rku.ac.in>
2025-03-05 16:45:16 +0530

    Rock Paper Scissors

--- RockScissorsPaper/script.js
+++ RockScissorsPaper/script.js
@@ -8,12 +8,22 @@ optionImages.forEach((image,index)=>{
     image.addEventListener('click',(e)=>{
         image.classList.add('active');
 
+        userResult.src = cpuResult.src = "images/rock.png";
+        result.textContent = "wait...";
 
         optionImages.forEach((image2,index2)=>{
             // index !== index2 && image2.classList.remove('active');
             index !== index2 && image2.classList.remove('active');
 
         })
+
+
+        gameContainer.classList.add("start");
+
+
+        let time = setTimeout(() =>{
+
+            gameContainer.classList.remove("start");
 
             let imageSrc = e.target.querySelector('.img').src;
             userResult.src = imageSrc;
@@ -23,7 +33,33 @@ optionImages.forEach((image,index)=>{
         // console.log(imageSrc);
         // userResult.src = imageSrc;
 
-        // let randomNumber = Math.floor(Math.random()*3);
-        // console.log(randomNumber);
+        let randomNumber = Math.floor(Math.random()*3);
+        let cpuImages = ["images/rock.png", "images/paper.png", "images/scissors.png"];
+        cpuResult.src = cpuImages[randomNumber];
+
+
+        let cpuValue = ["R","P","S"][randomNumber];
+
+        let userValue = ["R","P","S"][randomNumber];
+
+
+        let outcomes = {
+            RR:"Draw",
+            RP:"Cpu",
+            RS:"User",
+            PP:"Draw",
+            PR:"User",
+            PS:"Cpu",
+            SS:"Draw",
+            SR:"Cpu",
+            SP:"User"
+
+        }
+        let outComeValue = outcomes[userValue+cpuValue];
+        result.textContent = userValue === cpuValue ? "Draw" :  `${outComeValue} Won!!`;
+        // console.log(cpuResult,userValue);
+        },2500)
+
+        
     })
 })
